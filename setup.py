44d97bc8be824aaf212608cac9c61ba3ccbb6269
Niccolò Zanotti <niccolo.zanotti@protonmail.com>
2025-05-15 08:22:05 +0100

    Add development dependencies to `dev` optional group

--- setup.py
+++ setup.py
@@ -9,4 +9,12 @@ setup(
     cmake_install_dir=".",
     python_requires=">=3.12",
     install_requires=["numpy>=2.0"],
+    extras_require={
+        "dev": [
+            "cmakelang>=0.6.13",
+            "fortitude-lint>=0.7.3",
+            "ruff>=0.11.9",
+            "zizmor>=1.7.0",
+        ]
+    },
 )
